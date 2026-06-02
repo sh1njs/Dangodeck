714a7435c12b3034d4d3306b10b011567c052f26
root <root@shieun.servarica.net>
2026-06-02 23:28:51 +0000

    feat: refactor struktur

--- dist/public/nav.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var toggle = document.querySelector('[data-nav-toggle]');
-  var mobile = document.querySelector('[data-nav-mobile]');
-  if (!toggle || !mobile) return;
-  toggle.addEventListener('click', function () {
-    mobile.hidden = !mobile.hidden;
-  });
-})();
